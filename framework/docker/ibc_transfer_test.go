ab5e1e8420b4ec0f354581dd208679ea2b3f5dc4
chatton <github.qpeyb@simplelogin.fr>
2025-08-19 08:32:33 +0200

    wip: extracting ibc tests

--- framework/docker/ibc_transfer_test.go
+++ framework/docker/ibc_transfer_test.go
@@ -3,17 +3,91 @@ package docker
 import (
 	"context"
 	sdkmath "cosmossdk.io/math"
+	"github.com/celestiaorg/tastora/framework/docker/ibc"
+	"github.com/celestiaorg/tastora/framework/docker/ibc/relayer"
 	"github.com/celestiaorg/tastora/framework/testutil/query"
 	"github.com/celestiaorg/tastora/framework/testutil/sdkacc"
 	"github.com/celestiaorg/tastora/framework/testutil/wait"
 	"github.com/celestiaorg/tastora/framework/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module/testutil"
+	"github.com/cosmos/cosmos-sdk/x/auth"
+	"github.com/cosmos/cosmos-sdk/x/bank"
+	"github.com/cosmos/ibc-go/v8/modules/apps/transfer"
 	ibctransfertypes "github.com/cosmos/ibc-go/v8/modules/apps/transfer/types"
 	clienttypes "github.com/cosmos/ibc-go/v8/modules/core/02-client/types"
+	dockerclient "github.com/moby/moby/client"
+	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
+	"go.uber.org/zap/zaptest"
+	"golang.org/x/sync/errgroup"
 	"testing"
 	"time"
 )
+
+// IBCTestSetupConfig contains all the components needed for a Docker IBC test
+type IBCTestSetupConfig struct {
+	TestSetupConfig
+
+	// IBC-specific components
+	chainA     types.Chain // celestia-app chain
+	chainB     types.Chain // simapp chain
+	relayer    *relayer.Hermes
+	connection ibc.Connection
+	channel    ibc.Channel
+}
+
+func setupIBCDockerTest(t *testing.T) *IBCTestSetupConfig {
+	ctx := context.Background()
+	logger := zaptest.NewLogger(t)
+	encConfig := testutil.MakeTestEncodingConfig(auth.AppModuleBasic{}, bank.AppModuleBasic{}, transfer.AppModuleBasic{})
+
+	dockerClient, networkID := DockerSetup(t)
+
+	// Create celestia-app chain (chain A)
+	chainA, err := createCelestiaChain(t, ctx, dockerClient, networkID, encConfig)
+	require.NoError(t, err, "failed to create celestia chain")
+
+	// Create simapp chain (chain B)
+	chainB, err := createSimappChain(t, ctx, dockerClient, networkID, encConfig)
+	require.NoError(t, err, "failed to create simapp chain")
+
+	// Start both chains in parallel
+	eg, egCtx := errgroup.WithContext(ctx)
+	eg.Go(func() error {
+		return chainA.Start(egCtx)
+	})
+	eg.Go(func() error {
+		return chainB.Start(egCtx)
+	})
+	require.NoError(t, eg.Wait(), "failed to start chains")
+
+	// Create and initialize relayer (but don't start it)
+	hermes, err := relayer.NewHermes(ctx, dockerClient, t.Name(), networkID, 0, logger)
+	require.NoError(t, err, "failed to create hermes relayer")
+
+	err = hermes.Init(ctx, chainA, chainB)
+	require.NoError(t, err, "failed to initialize relayer")
+
+	// Setup IBC connection and channel
+	connection, channel := setupIBCConnection(t, ctx, chainA, chainA, hermes)
+
+	ibcCfg := &IBCTestSetupConfig{
+		TestSetupConfig: TestSetupConfig{
+			DockerClient: dockerClient,
+			NetworkID:    networkID,
+			Logger:       logger,
+			EncConfig:    encConfig,
+		},
+		chainA:     chainA,
+		chainB:     chainB,
+		relayer:    hermes,
+		connection: connection,
+		channel:    channel,
+	}
+
+	return ibcCfg
+}
 
 // TestIBCTransfer tests a complete IBC token transfer between celestia-app and simapp
 func (s *IBCTestSuite) TestIBCTransfer() {
