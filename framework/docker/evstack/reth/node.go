4fa53ee0ad1b600475eb38d5f0396dcf45ed484a
chatton <github.qpeyb@simplelogin.fr>
2025-09-18 15:23:14 +0100

    chore: strip out unused code

--- framework/docker/evstack/reth/node.go
+++ framework/docker/evstack/reth/node.go
@@ -41,7 +41,7 @@ type NodeConfig struct {
 type NodeConfigBuilder struct{ cfg *NodeConfig }
 
 func NewNodeConfigBuilder() *NodeConfigBuilder {
-    return &NodeConfigBuilder{cfg: &NodeConfig{}}
+	return &NodeConfigBuilder{cfg: &NodeConfig{}}
 }
 func (b *NodeConfigBuilder) WithImage(img container.Image) *NodeConfigBuilder {
 	b.cfg.Image = &img
@@ -52,8 +52,8 @@ func (b *NodeConfigBuilder) WithAdditionalStartArgs(args ...string) *NodeConfigB
 	return b
 }
 func (b *NodeConfigBuilder) WithEnv(env ...string) *NodeConfigBuilder {
-    b.cfg.Env = env
-    return b
+	b.cfg.Env = env
+	return b
 }
 func (b *NodeConfigBuilder) WithInternalPorts(ports types.Ports) *NodeConfigBuilder {
 	b.cfg.InternalPorts = &ports
@@ -72,7 +72,7 @@ func (b *NodeConfigBuilder) WithAdditionalInitArgs(args ...string) *NodeConfigBu
 	return b
 }
 func (b *NodeConfigBuilder) Build() NodeConfig {
-    return *b.cfg
+	return *b.cfg
 }
 
 // Node represents a reth node container and its configuration.
@@ -91,27 +91,27 @@ type Node struct {
 }
 
 func newNode(cfg Config, testName string, index int, nodeCfg NodeConfig) *Node {
-    image := cfg.Image
-    if nodeCfg.Image != nil {
-        image = *nodeCfg.Image
-    }
+	image := cfg.Image
+	if nodeCfg.Image != nil {
+		image = *nodeCfg.Image
+	}
 
-    ports := defaultPorts()
-    if nodeCfg.InternalPorts != nil {
-        ports = *nodeCfg.InternalPorts
-    }
+	ports := defaultPorts()
+	if nodeCfg.InternalPorts != nil {
+		ports = *nodeCfg.InternalPorts
+	}
 
 	log := cfg.Logger.With(zap.String("component", "reth-node"), zap.Int("i", index))
 
 	homeDir := "/home/ev-reth"
 
-    n := &Node{
-        cfg:       cfg,
-        nodeCfg:   nodeCfg,
-        logger:    log,
-        internal:  ports,
-        genesisBz: nodeCfg.GenesisBz,
-    }
+	n := &Node{
+		cfg:       cfg,
+		nodeCfg:   nodeCfg,
+		logger:    log,
+		internal:  ports,
+		genesisBz: nodeCfg.GenesisBz,
+	}
 	n.Node = container.NewNode(cfg.DockerNetworkID, cfg.DockerClient, testName, image, homeDir, index, rethNodeType("node"), log)
 	n.SetContainerLifecycle(container.NewLifecycle(cfg.Logger, cfg.DockerClient, n.Name()))
 	return n
@@ -270,44 +270,6 @@ func (n *Node) writeNodeFiles(ctx context.Context) error {
 		}
 	}
 	return nil
-}
-
-// generateGenesis runs `ev-reth dump-genesis` and returns the JSON bytes.
-func (n *Node) generateGenesis(ctx context.Context) ([]byte, error) {
-	// Build env: chain + node
-	var env []string
-	env = append(env, n.cfg.Env...)
-	env = append(env, n.nodeCfg.Env...)
-
-	// Build args: allow AdditionalInitArgs to be passed through
-	cmd := []string{n.cfg.Bin, "dump-genesis"}
-	if len(n.cfg.AdditionalInitArgs) > 0 {
-		cmd = append(cmd, n.cfg.AdditionalInitArgs...)
-	}
-	if len(n.nodeCfg.AdditionalInitArgs) > 0 {
-		cmd = append(cmd, n.nodeCfg.AdditionalInitArgs...)
-	}
-
-	// Ensure a chain is specified; default to dev if none provided.
-	hasChain := false
-	for _, a := range cmd {
-		if a == "--chain" || len(a) >= 8 && a[:8] == "--chain=" {
-			hasChain = true
-			break
-		}
-	}
-	if !hasChain {
-		cmd = append(cmd, "--chain", "dev")
-	}
-
-	stdout, stderr, err := n.Exec(ctx, n.Logger, cmd, env)
-	if err != nil {
-		return nil, fmt.Errorf("dump-genesis failed: %w (stderr=%s)", err, string(stderr))
-	}
-	if len(stdout) == 0 || (len(stdout) > 0 && stdout[0] != '{') {
-		return nil, fmt.Errorf("dump-genesis returned unexpected output (len=%d)", len(stdout))
-	}
-	return stdout, nil
 }
 
 func (n *Node) createNodeContainer(ctx context.Context) error {
