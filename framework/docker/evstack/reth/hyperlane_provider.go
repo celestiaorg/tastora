cefca1de32bc0a8c46ac3a913d81c8b9dc0c94cb
chatton <github.qpeyb@simplelogin.fr>
2025-12-03 11:58:09 +0000

    chore: cleaned up reth implementation

--- framework/docker/evstack/reth/hyperlane_provider.go
+++ framework/docker/evstack/reth/hyperlane_provider.go
@@ -1,72 +1,99 @@
 package reth
 
 import (
-    "context"
-    "github.com/celestiaorg/tastora/framework/docker/hyperlane"
+	"context"
+	"fmt"
+	"github.com/celestiaorg/tastora/framework/docker/hyperlane"
 )
 
-// Ensure Node satisfies the new interface.
-// var _ hyperlane.ChainConfigProvider = (*Node)(nil)
+var _ hyperlane.ChainConfigProvider = (*Node)(nil)
 
 func (n *Node) GetHyperlaneRegistryMetadata(ctx context.Context) (hyperlane.ChainMetadata, error) {
-    return n.GetHyperlaneChainMetadata(ctx)
+	networkInfo, err := n.GetNetworkInfo(ctx)
+	if err != nil {
+		return hyperlane.ChainMetadata{}, fmt.Errorf("get network info: %w", err)
+	}
+
+	rpcURL := fmt.Sprintf("http://%s", networkInfo.Internal.RPCAddress())
+
+	return hyperlane.ChainMetadata{
+		ChainID:     1234, // hard coded to the value in the default genesis.
+		DomainID:    1234,
+		Name:        "rethlocal",
+		DisplayName: "Reth",
+		Protocol:    "ethereum",
+		IsTestnet:   true,
+		NativeToken: hyperlane.NativeToken{
+			Name:     "Ether",
+			Symbol:   "ETH",
+			Decimals: 18,
+		},
+		RpcURLs: []hyperlane.Endpoint{
+			{HTTP: rpcURL},
+		},
+		Blocks: &hyperlane.BlockConfig{
+			Confirmations:     1,
+			EstimateBlockTime: 3,
+			ReorgPeriod:       0,
+		},
+		// NOTE: this key is hard coded for testing purposes and corresponds to the 0xaF9053bB6c4346381C77C2FeD279B17ABAfCDf4d
+		// in the default genesis used.
+		SignerKey: "0x82bfcfadbf1712f6550d8d2c00a39f05b33ec78939d0167be2a737d691f33a6a",
+		CoreContracts: &hyperlane.CoreContractAddresses{
+			Mailbox:                  "0xb1c938F5BA4B3593377F399e12175e8db0C787Ff",
+			InterchainSecurityModule: "0xa05915fD6E32A1AA7E67d800164CaCB12487142d",
+			InterchainGasPaymaster:   "0x1D957dA7A6988f5a9d2D2454637B4B7fea0Aeea5",
+			MerkleTreeHook:           "0xFCb1d485ef46344029D9E8A7925925e146B3430E",
+			ProxyAdmin:               "0x7e7aD18Adc99b94d4c728fDf13D4dE97B926A0D8",
+			ValidatorAnnounce:        "0x79ec7bF05AF122D3782934d4Fb94eE32f0C01c97",
+			AggregationHook:          "0xe53275A1FcA119e1c5eeB32E7a72e54835A63936",
+			DomainRoutingIsm:         "0xE2c1756b8825C54638f98425c113b51730cc47f6",
+			FallbackRoutingHook:      "0xE2c1756b8825C54638f98425c113b51730cc47f6",
+			ProtocolFee:              "0x8A93d247134d91e0de6f96547cB0204e5BE8e5D8",
+			StorageGasOracle:         "0x457cCf29090fe5A24c19c1bc95F492168C0EaFdb",
+			TestRecipient:            "0xd7958B336f0019081Ad2279B2B7B7c3f744Bce0a",
+		},
+	}, nil
+
 }
 
 func (n *Node) GetHyperlaneRelayerChainConfig(ctx context.Context) (hyperlane.RelayerChainConfig, error) {
-    meta, err := n.GetHyperlaneChainMetadata(ctx)
-    if err != nil {
-        return hyperlane.RelayerChainConfig{}, err
-    }
+	meta, err := n.GetHyperlaneRegistryMetadata(ctx)
+	if err != nil {
+		return hyperlane.RelayerChainConfig{}, err
+	}
 
-    cfg := hyperlane.RelayerChainConfig{
-        Name:        meta.Name,
-        ChainID:     meta.ChainID,
-        DomainID:    meta.DomainID,
-        DisplayName: meta.DisplayName,
-        Protocol:    meta.Protocol,
-        IsTestnet:   meta.IsTestnet,
-        NativeToken: &meta.NativeToken,
-        Blocks:      meta.Blocks,
-        RpcURLs:     meta.RpcURLs,
-    }
+	cfg := hyperlane.RelayerChainConfig{
+		Name:        meta.Name,
+		ChainID:     meta.ChainID,
+		DomainID:    meta.DomainID,
+		DisplayName: meta.DisplayName,
+		Protocol:    meta.Protocol,
+		IsTestnet:   meta.IsTestnet,
+		NativeToken: &meta.NativeToken,
+		Blocks:      meta.Blocks,
+		RpcURLs:     meta.RpcURLs,
+	}
 
-    // signer
-    signer := &hyperlane.SignerConfig{Key: meta.SignerKey}
-    if meta.Protocol == "ethereum" {
-        signer.Type = "hexKey"
-    } else if meta.Protocol == "cosmosnative" {
-        signer.Type = "cosmosKey"
-        signer.Prefix = meta.Bech32Prefix
-    }
-    cfg.Signer = signer
+	cfg.Signer = &hyperlane.SignerConfig{
+		Key:  meta.SignerKey,
+		Type: "hexKey",
+		// no bech32 prefix required for evm.
+	}
 
-    // protocol-specific fields
-    if meta.Protocol == "ethereum" && meta.CoreContracts != nil {
-        cfg.Mailbox = meta.CoreContracts.Mailbox
-        cfg.InterchainSecurityModule = meta.CoreContracts.InterchainSecurityModule
-        cfg.InterchainGasPaymaster = meta.CoreContracts.InterchainGasPaymaster
-        cfg.MerkleTreeHook = meta.CoreContracts.MerkleTreeHook
-        cfg.ProxyAdmin = meta.CoreContracts.ProxyAdmin
-        cfg.ValidatorAnnounce = meta.CoreContracts.ValidatorAnnounce
-        cfg.AggregationHook = meta.CoreContracts.AggregationHook
-        cfg.DomainRoutingIsm = meta.CoreContracts.DomainRoutingIsm
-        cfg.FallbackRoutingHook = meta.CoreContracts.FallbackRoutingHook
-        cfg.ProtocolFee = meta.CoreContracts.ProtocolFee
-        cfg.StorageGasOracle = meta.CoreContracts.StorageGasOracle
-        cfg.TestRecipient = meta.CoreContracts.TestRecipient
-    }
+	// protocol-specific fields
+	cfg.Mailbox = meta.CoreContracts.Mailbox
+	cfg.InterchainSecurityModule = meta.CoreContracts.InterchainSecurityModule
+	cfg.InterchainGasPaymaster = meta.CoreContracts.InterchainGasPaymaster
+	cfg.MerkleTreeHook = meta.CoreContracts.MerkleTreeHook
+	cfg.ProxyAdmin = meta.CoreContracts.ProxyAdmin
+	cfg.ValidatorAnnounce = meta.CoreContracts.ValidatorAnnounce
+	cfg.AggregationHook = meta.CoreContracts.AggregationHook
+	cfg.DomainRoutingIsm = meta.CoreContracts.DomainRoutingIsm
+	cfg.FallbackRoutingHook = meta.CoreContracts.FallbackRoutingHook
+	cfg.ProtocolFee = meta.CoreContracts.ProtocolFee
+	cfg.StorageGasOracle = meta.CoreContracts.StorageGasOracle
+	cfg.TestRecipient = meta.CoreContracts.TestRecipient
 
-    if meta.Protocol == "cosmosnative" {
-        cfg.Bech32Prefix = meta.Bech32Prefix
-        cfg.CanonicalAsset = meta.CanonicalAsset
-        cfg.ContractAddressBytes = meta.ContractAddressBytes
-        cfg.RestURLs = meta.RestURLs
-        cfg.GrpcURLs = meta.GrpcURLs
-        cfg.GasPrice = meta.GasPrice
-        cfg.Index = meta.IndexConfig
-        cfg.Slip44 = meta.Slip44
-    }
-
-    return cfg, nil
+	return cfg, nil
 }
-
