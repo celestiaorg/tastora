cf3d1b568428839c9a14e048a1664e9447e90f54
chatton <github.qpeyb@simplelogin.fr>
2025-12-04 10:26:29 +0000

    chore: simplify test with deploy minimal stack helper fn

--- framework/docker/hyperlane_deploy_test.go
+++ framework/docker/hyperlane_deploy_test.go
@@ -3,17 +3,11 @@ package docker
 import (
 	"context"
 	"encoding/json"
-	"fmt"
-	"net"
-	"net/http"
 	"path/filepath"
 	"testing"
 	"time"
 
-	da "github.com/celestiaorg/tastora/framework/docker/dataavailability"
-	evmsingle "github.com/celestiaorg/tastora/framework/docker/evstack/evmsingle"
 	"github.com/celestiaorg/tastora/framework/docker/hyperlane"
-	"github.com/celestiaorg/tastora/framework/types"
 	commonpkg "github.com/ethereum/go-ethereum/common"
 	"github.com/stretchr/testify/require"
 )
@@ -29,114 +23,11 @@ func TestHyperlaneDeployer_Bootstrap(t *testing.T) {
 	ctx, cancel := context.WithTimeout(testCfg.Ctx, 5*time.Minute)
 	defer cancel()
 
-	// 1) Start a Celestia App chain (needed for DA network)
-	chain, err := testCfg.ChainBuilder.Build(ctx)
+	// Bring up full stack with defaults (celestia-app, DA bridge, reth, evm-single)
+	stack, err := DeployMinimalStack(t, testCfg)
 	require.NoError(t, err)
-	require.NoError(t, chain.Start(ctx))
-	t.Cleanup(func() { _ = chain.Remove(context.Background()) })
-
-	// 2) Start only the DA bridge node (minimal for this test) with custom network settings
-	chainID := chain.GetChainID()
-	chainNI, err := chain.GetNodes()[0].GetNetworkInfo(ctx)
-	require.NoError(t, err)
-	coreHost := chainNI.Internal.Hostname
-	genesisHash, err := getGenesisHash(ctx, chain)
-	require.NoError(t, err)
-
-	danet, err := testCfg.DANetworkBuilder.WithChainID(chainID).Build(ctx)
-	require.NoError(t, err)
-	bridge := danet.GetBridgeNodes()[0]
-	require.NoError(t, bridge.Start(ctx,
-		da.WithChainID(chainID),
-		da.WithAdditionalStartArguments("--p2p.network", chainID, "--core.ip", coreHost, "--rpc.addr", "0.0.0.0"),
-		da.WithEnvironmentVariables(map[string]string{
-			"CELESTIA_CUSTOM": types.BuildCelestiaCustomEnvVar(chainID, genesisHash, ""),
-			"P2P_NETWORK":     chainID,
-		}),
-	))
-	t.Cleanup(func() { _ = bridge.Remove(context.Background()) })
-
-	// compute DA address for evm-single (use bridge internal RPC)
-	bridgeNI, err := bridge.GetNetworkInfo(ctx)
-	require.NoError(t, err)
-	daAddress := fmt.Sprintf("http://%s:%s", bridgeNI.Internal.IP, bridgeNI.Internal.Ports.RPC)
-
-	// 3) Start a single reth node
-	rnode, err := testCfg.RethBuilder.Build(ctx)
-	require.NoError(t, err)
-	t.Cleanup(func() { _ = rnode.Remove(context.Background()) })
-	require.NoError(t, rnode.Start(ctx))
-
-	// Wait until reth RPC becomes responsive
-	require.Eventually(t, func() bool {
-		ec, err := rnode.GetEthClient(ctx)
-		if err != nil {
-			return false
-		}
-		_, err = ec.BlockNumber(ctx)
-		return err == nil
-	}, 45*time.Second, 1*time.Second)
-
-	// Ensure Engine (authrpc) TCP port is open on host mapping before starting evm-single
-	rni2, err := rnode.GetNetworkInfo(ctx)
-	require.NoError(t, err)
-	engineHost := fmt.Sprintf("0.0.0.0:%s", rni2.External.Ports.Engine)
-	require.Eventually(t, func() bool {
-		c, err := net.DialTimeout("tcp", engineHost, 2*time.Second)
-		if err != nil {
-			return false
-		}
-		_ = c.Close()
-		return true
-	}, 45*time.Second, 1*time.Second)
-
-	// Build URLs for evm-single
-	rethNetworkInfo, err := rnode.GetNetworkInfo(ctx)
-	require.NoError(t, err)
-	evmEthURL := fmt.Sprintf("http://%s:%s", rethNetworkInfo.Internal.Hostname, rethNetworkInfo.Internal.Ports.RPC)
-	evmEngineURL := fmt.Sprintf("http://%s:%s", rethNetworkInfo.Internal.Hostname, rethNetworkInfo.Internal.Ports.Engine)
-	// Fetch genesis block hash from reth
-	evmGenesisHash, err := rnode.GenesisHash(ctx)
-	require.NoError(t, err)
-
-	// Start evm-single linked to reth and DA
-	ebuilder := testCfg.EVMSingleChainBuilder.
-		WithNode(
-			evmsingle.NewNodeConfigBuilder().
-				WithEVMEngineURL(evmEngineURL).
-				WithEVMETHURL(evmEthURL).
-				WithEVMJWTSecret(rnode.JWTSecretHex()).
-				WithEVMSignerPassphrase("secret").
-				WithEVMBlockTime("1s").
-				WithEVMGenesisHash(evmGenesisHash).
-				WithDAAddress(daAddress).
-				Build(),
-		)
-
-	echain, err := ebuilder.Build(ctx)
-	require.NoError(t, err)
-	t.Cleanup(func() {
-		_ = echain.Stop(ctx)
-		_ = echain.Remove(ctx)
-	})
-	require.NoError(t, echain.Start(ctx))
-
-	// Health check via evnode health endpoint
-	enodes := echain.Nodes()
-	require.Len(t, enodes, 1)
-	eni, err := enodes[0].GetNetworkInfo(ctx)
-	require.NoError(t, err)
-	require.NotEmpty(t, eni.External.Ports.RPC)
-	healthURL := fmt.Sprintf("http://0.0.0.0:%s/health/ready", eni.External.Ports.RPC)
-	require.Eventually(t, func() bool {
-		req, _ := http.NewRequestWithContext(ctx, http.MethodGet, healthURL, nil)
-		resp, err := http.DefaultClient.Do(req)
-		if err != nil {
-			return false
-		}
-		defer func() { _ = resp.Body.Close() }()
-		return resp.StatusCode == http.StatusOK
-	}, 60*time.Second, 2*time.Second)
+	chain := stack.celestia
+	rnode := stack.reth
 
 	// 4) Initialize the Hyperlane deployer with the reth node as a provider
 	// Select a hyperlane image. Allow override via HYPERLANE_IMAGE (format repo:tag)
