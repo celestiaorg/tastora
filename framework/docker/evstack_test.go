49389000a2a47b679a7f5cd9761ec20c34db31ba
Cian Hatton <github.qpeyb@simplelogin.fr>
2025-12-04 10:53:32 +0000

    chore: add helpers to deploy parts of the stack (#155)
    * chore: add helper to deploy full stack
    
    * chore: add test helpers to reduce verbosity in tests
    
    * chore: switch from main to tag

--- framework/docker/evstack_test.go
+++ framework/docker/evstack_test.go
@@ -4,14 +4,8 @@ import (
 	"fmt"
 	"testing"
 
-	"cosmossdk.io/math"
 	"github.com/celestiaorg/tastora/framework/docker/container"
-	da "github.com/celestiaorg/tastora/framework/docker/dataavailability"
 	"github.com/celestiaorg/tastora/framework/docker/evstack"
-	sdkacc "github.com/celestiaorg/tastora/framework/testutil/sdkacc"
-	"github.com/celestiaorg/tastora/framework/types"
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	"github.com/stretchr/testify/require"
 )
 
@@ -25,73 +19,14 @@ func TestEvstack(t *testing.T) {
 	// Setup isolated docker environment for this test
 	testCfg := setupDockerTest(t)
 
-	chain, err := testCfg.ChainBuilder.Build(testCfg.Ctx)
+	_, daNetwork, err := DeployCelestiaWithDABridgeNode(t, testCfg)
 	require.NoError(t, err)
-
-	err = chain.Start(testCfg.Ctx)
-	require.NoError(t, err)
-
-	// Create DA network using builder pattern
-	celestiaImage := container.Image{
-		Repository: "ghcr.io/celestiaorg/celestia-node",
-		Version:    "v0.26.4",
-		UIDGID:     "10001:10001",
-	}
-
-	bridgeNodeConfig := da.NewNodeBuilder().
-		WithNodeType(types.BridgeNode).
-		Build()
-
-	daNetwork, err := testCfg.DANetworkBuilder.
-		WithChainID(chain.GetChainID()).
-		WithImage(celestiaImage).
-		WithNodes(bridgeNodeConfig).
-		Build(testCfg.Ctx)
-	require.NoError(t, err)
-
-	genesisHash, err := getGenesisHash(testCfg.Ctx, chain)
-	require.NoError(t, err)
-
-	networkInfo, err := chain.GetNodes()[0].GetNetworkInfo(testCfg.Ctx)
-	require.NoError(t, err, "failed to get network info")
-	hostname := networkInfo.Internal.Hostname
 
 	bridgeNode := daNetwork.GetBridgeNodes()[0]
-	chainID := chain.GetChainID()
-
-	t.Run("bridge node can be started", func(t *testing.T) {
-		err = bridgeNode.Start(testCfg.Ctx,
-			da.WithChainID(chainID),
-			da.WithAdditionalStartArguments("--p2p.network", chainID, "--core.ip", hostname, "--rpc.addr", "0.0.0.0"),
-			da.WithEnvironmentVariables(
-				map[string]string{
-					"CELESTIA_CUSTOM": types.BuildCelestiaCustomEnvVar(chainID, genesisHash, ""),
-					"P2P_NETWORK":     chainID,
-				},
-			),
-		)
-		require.NoError(t, err)
-	})
-
-	daWallet, err := bridgeNode.GetWallet()
-	require.NoError(t, err)
-	t.Logf("da node celestia address: %s", daWallet.GetFormattedAddress())
-
-	// Fund the da node address
-	fromAddress, err := sdkacc.AddressFromWallet(chain.GetFaucetWallet())
-	require.NoError(t, err)
-
-	toAddress, err := sdk.AccAddressFromBech32(daWallet.GetFormattedAddress())
-	require.NoError(t, err)
-
-	// Fund the ev node wallet with coins
-	bankSend := banktypes.NewMsgSend(fromAddress, toAddress, sdk.NewCoins(sdk.NewCoin("utia", math.NewInt(100_000_000_00))))
-	_, err = chain.BroadcastMessages(testCfg.Ctx, chain.GetFaucetWallet(), bankSend)
-	require.NoError(t, err)
 
 	evstackImage := container.Image{
 		Repository: "ghcr.io/evstack/ev-node",
-		Version:    "main",
+		Version:    "v1.0.0-beta.8",
 		UIDGID:     "10001:10001",
 	}
 
